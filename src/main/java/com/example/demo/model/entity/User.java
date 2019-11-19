46ec1ec7088a96a6c0f53020668a582880f7af44
HyungjinJeon <qpdjgjsl@gmail.com>
2019-11-19 16:39:34 +0900

    Lombok constructor patern

--- src/main/java/com/example/demo/model/entity/User.java
+++ src/main/java/com/example/demo/model/entity/User.java
@@ -18,16 +18,20 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.experimental.Accessors;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString(exclude = {"orderGroupList"})
-@EntityListeners(AuditingEntityListener.class )
+@EntityListeners(AuditingEntityListener.class)
+@Builder
+@Accessors(chain = true)
 public class User {
 
 	@Id
